cc4cc442dbd79876732da72b96933dee5a8d8ba4
Clemence Esp <c.esposito@ascan.io>
2020-02-27 13:49:03 +0100

    Test with build in folder

---
+++ build/precache-manifest.62eeb9a6edb79f4ea795bd070e3d5d88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad7d6e2938eac3f6862bde55aa81ae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aec38cc99f2cc0c4c283",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3d5475c8c64f1f2198b8",
+    "url": "/static/js/2.68bfedc5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.68bfedc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec38cc99f2cc0c4c283",
+    "url": "/static/js/main.7d6d70bd.chunk.js"
+  },
+  {
+    "revision": "925dd431e3f9c006e0d9",
+    "url": "/static/js/runtime-main.bf50ea06.js"
+  }
+]);
